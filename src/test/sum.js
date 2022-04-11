f2dd3d44f633216ad95732a96256763678edf139
Sage-Park <sagepark94@gmail.com>
2022-04-11 21:53:00 +0900

    update test

--- src/test/sum.js
+++ src/test/sum.js
@@ -2,4 +2,10 @@ function sum(a, b) {
     return a + b;
 }
 
-module.exports = sum;
+function sumOf(numbers) {
+    return numbers.reduce((acc, current) => acc + current, 0);
+}
+
+exports.sum = sum;
+exports.sumOf = sumOf;
+
--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -1,5 +1,17 @@
-const sum = require('./sum')
+const {sumOf, sum} = require("./sum");
 
 test('1 + 2 = 3', () => {
     expect(sum(1, 2)).toBe(3);
+})
+
+describe('sum', () => {
+    it('calculates 1 + 2', function () {
+        expect(sum(1, 2)).toBe(3);
+    });
+
+    it('calculates all numbers', function () {
+        const array = [1, 2, 3, 4, 5];
+
+        expect(sumOf(array)).toBe(15);
+    });
 })
